43b70418dafe41ba02c796db394ae895f42fc2f7
CaesarTsai <x24libra@hotmail.com>
2019-01-09 21:25:53 +0800

    98. Validate Binary Search Tree 錯誤解法2

--- 98. Validate Binary Search Tree/main.go
+++ 98. Validate Binary Search Tree/main.go
@@ -6,13 +6,17 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func isValidBST(root *TreeNode) bool {
+	return isValidBSTPassParent(root, nil)
+}
+
 func isValidBST_IncorrectSol(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
 
 	// 疏忽1. 沒考慮節點數值相同,此情況違反BST
-	// 疏忽2. 右子樹所有節點一定大於跟節點,不能只單純看單一node的情況
+	// 疏忽2. 右子樹所有節點一定大於根節點,不能只單純看單一node的情況
 	if (root.Left != nil) && (root.Left.Val >= root.Val) {
 		return false
 	}
@@ -24,19 +28,29 @@ func isValidBST_IncorrectSol(root *TreeNode) bool {
 	return isValidBST_IncorrectSol(root.Left) || isValidBST_IncorrectSol(root.Right)
 }
 
-
-func isValidBST(root *TreeNode) bool {
+func isValidBSTPassParent(root, parent *TreeNode) bool {
 	if root == nil {
 		return true
 	}
 
-	if (root.Left != nil) && (root.Left.Val >= root.Val) {
-		return false
+	if root.Left != nil {
+		if root.Left.Val >= root.Val {
+			return false
+		}
+		// 疏忽3. 右子樹所有節點一定大於左邊所有節點
+		if parent != nil && root.Left.Val >= parent.Val {
+			return false
+		}
 	}
 
-	if (root.Right != nil) && (root.Right.Val <= root.Val) {
-		return false
+	if root.Right != nil {
+		if root.Right.Val <= root.Val {
+			return false
+		}
+		if parent != nil && root.Right.Val <= parent.Val {
+			return false
+		}
 	}
 
-	return isValidBST(root.Left) || isValidBST(root.Right)
+	return isValidBSTPassParent(root.Left, root) || isValidBSTPassParent(root.Right, root)
 }
