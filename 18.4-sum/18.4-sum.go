272d32af173d2dccb26b0a3dfd3aa9bd873a9fb7
caesar <x246libra@hotmail.com>
2021-06-15 00:06:18 +0800

    18. 4Sum: 利用 twoSum 想法求解

--- 18.4-sum/18.4-sum.go
+++ 18.4-sum/18.4-sum.go
@@ -15,7 +15,7 @@ import (
 
 func main() {
 	// result := fourSumV2([]int{-500, -481, -480, -469, -437, -423, -408, -403, -397, -381, -379, -377, -353, -347, -337, -327, -313, -307, -299, -278, -265, -258, -235, -227, -225, -193, -192, -177, -176, -173, -170, -164, -162, -157, -147, -118, -115, -83, -64, -46, -36, -35, -11, 0, 0, 33, 40, 51, 54, 74, 93, 101, 104, 105, 112, 112, 116, 129, 133, 146, 152, 157, 158, 166, 177, 183, 186, 220, 263, 273, 320, 328, 332, 356, 357, 363, 372, 397, 399, 420, 422, 429, 433, 451, 464, 484, 485, 498, 499}, 2139)
-	result := fourSumV2([]int{-497, -494, -484, -477, -453, -453, -444, -442, -428, -420, -401, -393, -392, -381, -357, -357, -327, -323, -306, -285, -284, -263, -262, -254, -243, -234, -208, -170, -166, -162, -158, -136, -133, -130, -119, -114, -101, -100, -86, -66, -65, -6, 1, 3, 4, 11, 69, 77, 78, 107, 108, 108, 121, 123, 136, 137, 151, 153, 155, 166, 170, 175, 179, 211, 230, 251, 255, 266, 288, 306, 308, 310, 314, 321, 322, 331, 333, 334, 347, 349, 356, 357, 360, 361, 361, 367, 375, 378, 387, 387, 408, 414, 421, 435, 439, 440, 441, 470, 492}, 1682)
+	result := fourSumV5([]int{-497, -494, -484, -477, -453, -453, -444, -442, -428, -420, -401, -393, -392, -381, -357, -357, -327, -323, -306, -285, -284, -263, -262, -254, -243, -234, -208, -170, -166, -162, -158, -136, -133, -130, -119, -114, -101, -100, -86, -66, -65, -6, 1, 3, 4, 11, 69, 77, 78, 107, 108, 108, 121, 123, 136, 137, 151, 153, 155, 166, 170, 175, 179, 211, 230, 251, 255, 266, 288, 306, 308, 310, 314, 321, 322, 331, 333, 334, 347, 349, 356, 357, 360, 361, 361, 367, 375, 378, 387, 387, 408, 414, 421, 435, 439, 440, 441, 470, 492}, 1682)
 	// result := fourSumV2([]int{1, 0, -1, 0, -2, 2}, 0)
 	fmt.Println(result)
 }
@@ -180,11 +180,11 @@ func fourSumV3(nums []int, target int) [][]int {
 	return result
 }
 
-func isDuplicate(result [][]int, single []int) bool {
+func isDuplicate(result [][]int, subResult []int) bool {
 	for _, list := range result {
 		same := 0
 		for i, v := range list {
-			if single[i] == v {
+			if subResult[i] == v {
 				same++
 			}
 		}
@@ -221,5 +221,72 @@ func fourSumV4(nums []int, target int) [][]int {
 			}
 		}
 	}
+	return result
+}
+
+// Runtime: 196 ms, faster than 8.89% of Go online submissions for 4Sum.
+// Memory Usage: 4 MB, less than 21.33% of Go online submissions for 4Sum.
+//
+// 只能對付 4sum
+func fourSumV5(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+	n := len(nums)
+	record := make(map[[4]int]bool)
+
+	// two sum variable
+	var subTarget, start int
+	var num, remainder int
+	subRecord := make(map[int]bool)
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+
+			// two sum
+			subTarget = target - nums[i] - nums[j]
+			start = j + 1
+			for k := start; k < n; k++ {
+				num = nums[k]
+				remainder = subTarget - num
+				if subRecord[remainder] {
+					r := [4]int{nums[i], nums[j], remainder, num}
+					if !record[r] {
+						result = append(result, r[:])
+						record[r] = true
+					}
+				} else {
+					subRecord[num] = true
+				}
+			}
+
+			for key := range subRecord {
+				delete(subRecord, key)
+			}
+
+		}
+	}
+	return result
+}
+
+// 在 V5 版本, 若把 two sum 抽成函數放在外面呼叫
+// 會因為 反覆建立 map 結構, 導致 效能大幅降低
+func twoSum(nums []int, target int, start int) [][]int {
+	n := len(nums)
+	stat := make(map[int]bool)
+	var current, remainder int
+	var result [][]int
+
+	for k := start; k < n; k++ {
+		current = nums[k]
+		remainder = target - current
+		if stat[remainder] {
+			// 可能存在多個解, 不能直接回傳
+			// return []int{remainder, current}
+			result = append(result, []int{remainder, current})
+		} else {
+			stat[current] = true
+		}
+	}
+
 	return result
 }
